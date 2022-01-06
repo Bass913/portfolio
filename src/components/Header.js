dd9f9337a730adeb3627334f7f507d19ab8d02c0
Bass913 <Bastiendikiadi@outlook.fr>
2022-01-06 18:14:23 +0100

    Project Completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Typed from 'react-typed'
+import { Link } from 'react-scroll'
 
 const Header = () => {
     return (
@@ -13,7 +14,7 @@ const Header = () => {
                  backSpeed={60}
                  loop
                  />
-                 <a href='/#' className='btn-main-offer'>Contactez-moi</a>
+                 <Link to='contacts' offset={-175} className='btn-main-offer'>Contactez-moi</Link>
             </div>  
         </div>
     )
